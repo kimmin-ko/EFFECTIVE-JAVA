db0b1bb7ad47a3db001f5cd4eb77582436304c9f
km9202 <kimmin2301@gmail.com>
2020-06-17 19:40:21 +0900

    Initial commit

---
+++ src/chap02/ITEM_1.java
@@ -0,0 +1,13 @@
+package chap02;
+
+public class ITEM_1 {
+
+    public ITEM_1() {
+
+    }
+
+    public static Boolean valueOf(boolean b) {
+        return b ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+}
